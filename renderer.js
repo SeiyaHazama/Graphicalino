4d04a58b23f2bc2890a995a502275e7579a0c118
Seiya Hazama <zama_8722@live.jp>
2019-06-16 11:28:20 +0900

    Add func of some slect port

--- renderer.js
+++ renderer.js
@@ -1,8 +1,10 @@
 const ipcRenderer = require("electron").ipcRenderer;
 const portsElm = document.getElementById("ports");
 
+let conPort = "";
+
 document.addEventListener('DOMContentLoaded', () => {
-  ipcRenderer.send('port', 'request');
+  loadSerialPortList();
 });
 
 ipcRenderer.on('port', (event, msg) => {
@@ -10,7 +12,7 @@ ipcRenderer.on('port', (event, msg) => {
   let flg = false;
   for (var i = 0; i < msg.length; i++) {
     if (msg[i].manufacturer && msg[i].manufacturer.match("Arduino") && !flg) {
-      dispNotification("alert alert-primary", "Arduinoを検出し、自動選択しました。");
+      dispNotification("alert alert-success", "Arduinoを検出し、自動選択しました。");
       portsElm.innerHTML += '<option selected value="' + msg[i].comName + '">' + msg[i].comName + '</option>';
       flg = true;
     } else {
@@ -19,11 +21,32 @@ ipcRenderer.on('port', (event, msg) => {
   }
   portsElm.innerHTML += '<option value="reload">ポートを更新</option>';
   if (!flg) {
-    dispNotification("alert alert-warning", "Arduinoを見つけることができませんでした。<br>互換品の場合は手動で選択してください。");
+    dispNotification("alert alert-warning", "接続先が選択されていません");
   }
 });
 
 function dispNotification(cls, text) {
   document.getElementById("notif").className = cls;
   document.getElementById("notif").innerHTML = text;
+}
+
+function loadSerialPortList() {
+  dispNotification("","");
+  portsElm.innerHTML = '<option selected>お待ちください...</option>';
+  ipcRenderer.send('port', 'request');
+}
+
+function changedSelectPort() {
+  switch (document.getElementById("ports").value) {
+    case "reload":
+      loadSerialPortList();
+      break;
+    case "none":
+      conPort = "";
+      dispNotification("alert alert-warning", "接続先が選択されていません");
+      break;
+    default:
+      conPort = document.getElementById("ports").value;
+      dispNotification("alert alert-primary", "Arduinoであることを確認し[接続]をクリックします");
+  }
 }
